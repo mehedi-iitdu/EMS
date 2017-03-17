f86a89fc427fce21d2ec3bc281023c4067642035
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-03-18 02:28:27 +0600

    Sorting added in role and department page also

--- role.php
+++ role.php
@@ -89,6 +89,7 @@
 
 <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/stupidtable.js"></script>
 
 <script type="text/javascript">
 	
